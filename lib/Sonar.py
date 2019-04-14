01a5790738875f576659e3eed139b792cb465bc6
Pavel Šeps <pavel.seps@layoutmaschine.de>
2019-04-14 17:15:03 +0200

    prepare sonar reads

--- lib/Sonar.py
+++ lib/Sonar.py
@@ -1,40 +1,53 @@
+import math
 from time import sleep
 from threading import Thread
+from ev3dev2.motor import OUTPUT_C
+from ev3dev2.motor import MediumMotor
+from ev3dev2.sensor.lego import InfraredSensor
 
 
 class Sonar:
 #TODO implement scanning space in front of robot
     def __init__(self):
-        self.array = [0, 0, 0]
+        self.tempDistance = []
+        self.actualSector = 0
+        self.speed = 5
         self.segments = 5
+        self.distanceSegments = [0] * self.segments
         self.view_angle = 110
+        self.Motor = MediumMotor(OUTPUT_C)
+        self.ISensor = InfraredSensor()
 
     def thread_distance_values(self):
-        #TODO read angle of motor and set value in array
         while True:
-            if self.array[0] > 80:
-                self.array[0] = 0
-            if self.array[1] > 80:
-                self.array[1] = 0
-            if self.array[2] > 80:
-                self.array[2] = 0
+            print(self.Motor.position)  # TODO check if is this correct degrees
 
-            self.array[0] = self.array[0] + 1
-            self.array[1] = self.array[1] + 2
-            self.array[2] = self.array[2] + 3
-            sleep(1)
+            # sector = math.floor(self.Motor.position + (self.view_angle / 2) / (self.view_angle / self.segments))
+            #
+            # if sector != self.actualSector:
+            #     self.array[self.actualSector] = math.fsum(self.tempDistance) / len(self.tempDistance)
+            #     self.tempDistance = []
+            #     self.actualSector = sector
+            #
+            # self.tempDistance.append(self.ISensor.value())
+            # sleep(0.1)
 
     def thread_motor_rotation(self):
-        pass
-        #TODO rotate motor
+        self.Motor.on_for_degrees(speed=self.speed, degrees=-self.view_angle / self.segments)
+        while True:
+            self.Motor.on_for_degrees(speed=self.speed, degrees=self.view_angle)
+            self.Motor.on_for_degrees(speed=self.speed, degrees=-self.view_angle)
 
     def run(self):
         t1 = Thread(target=self.thread_distance_values)
+        t2 = Thread(target=self.thread_motor_rotation)
         t1.setDaemon(True)
+        t2.setDaemon(True)
         t1.start()
+        t2.start()
 
     def get_distance(self):
-        return self.array
+        return self.distanceSegments
 
     def get_segments(self):
         return self.segments
